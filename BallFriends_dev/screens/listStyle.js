75122771e9193e19ecef8653a1ff3472ff929d9a
JackalCheung <yhcheung57-c@my.cityu.edu.hk>
2021-02-01 00:33:02 +0800

    dev update (01/02/2021)
    Application front-end update at 01/02/2021:
    Fixtures List of Friendly and League pages in development.
    Displaying fixtures successfully.
    Fetching data from API-football is prevented with comment lines to avoid waste of fetching quota at this moment. (Can un-comment at anytime)
    
    Next update will be fixture details.

---
+++ BallFriends_dev/screens/listStyle.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from 'react-native';
+
+const listStyle = StyleSheet.create({
+    categoryContainer: {
+        flex: 1,
+        backgroundColor: "#ecf0f1",
+        padding: 5,
+        alignItems: "center",
+    },
+    categoryCard: {
+        width: "95%",
+        backgroundColor: "white",
+        borderRadius: 10,
+        elevation: 5,
+        padding: 3,
+    },
+    title: {
+        textAlign: "left",
+        padding: 5
+    },
+    itemContainer: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "white",
+        padding: 5,
+        alignItems: "center",
+    },
+    itemCard: {
+        width: "95%",
+        backgroundColor: "white",
+        padding: 3,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    homeTeam: {
+        flex: 2,
+        textAlign: "right",
+    },
+    awayTeam: {
+        flex: 2,
+        textAlign: "left",
+    },
+    time: {
+        flex: 1,
+        textAlign: "center",
+    },
+    logo: {
+        flex: 1,
+        width: 30,
+        height: 30,
+        resizeMode: "contain",
+        alignItems: "center"
+    }
+});
+
+export default listStyle;
